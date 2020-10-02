f878caddb3a0960ea3c8687b687c6b36eee71850
Christorres392 <christorres09@gmail.com>
2020-10-02 02:08:58 -0600

    saving before i try something

--- front-end/src/Movie.js
+++ front-end/src/Movie.js
@@ -5,16 +5,15 @@ class Movie extends Component {
         movie: ""
     }
 
-
-
-
     render() {
-        console.log(this.props.movie.title)
+
         return (
             <div>
                 <h1>{this.props.movie.title}</h1>
                 <img className="movie-image" src={`https://image.tmdb.org/t/p/original${this.props.movie.backdrop_path}`}></img>
-                 <p>{this.props.movie.overview}</p>
+                <div className="description-box">
+                    <p>{this.props.movie.overview}</p>
+                </div>
             </div>
         );
     }
--- front-end/src/containers/SearchResultBar.js
+++ front-end/src/containers/SearchResultBar.js
@@ -4,7 +4,7 @@ import MovieCard from '../MovieCard'
 const SearchResultBar = (props) => {
     return (
         <div className="row">
-            {props.searchMovies.map(movies => <MovieCard key={movies.id} movies={movies} />)}
+            {props.searchMovies.map(movies => <MovieCard handleClick={props.showCard} key={movies.id} movies={movies} />)}
         </div>
     );
 }
